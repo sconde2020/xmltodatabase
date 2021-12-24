7bebc8077e3c4d53a0cbe350445b6381b58c888e
sconde2020 <74711039+sconde2020@users.noreply.github.com>
2021-12-24 13:12:11 +0100

    Initializing the project

---
+++ xmltodatabase/src/main/java/com/sconde/xmltodatabase/XmltodatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.sconde.xmltodatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmltodatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmltodatabaseApplication.class, args);
+	}
+
+}
---
+++ xmltodatabase/src/test/java/com/sconde/xmltodatabase/XmltodatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sconde.xmltodatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmltodatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
